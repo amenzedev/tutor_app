bffa524cab2ed94d58850d7a0263eadb680b0333
amenzedev <amageb2014@gmail.com>
2025-05-08 17:40:24 -0400

    applied migration

--- tutor_app/tutor_app/urls.py
+++ tutor_app/tutor_app/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import register, user_login, user_logout
+from home.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
